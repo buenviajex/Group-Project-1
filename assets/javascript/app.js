88f1e61b27ae247931ec8608b1f1e6120c6375f0
matteichler <matteichler7@gmail.com>
2018-07-17 21:00:16 -0700

    google images search added - beta

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,28 @@ $(".searchButton").on("click", function(event) {
     var searchValue= $(".searchInput").val().trim();
 
     var GIapi = "AIzaSyBFF-Z0wLlFirA44q-R_Yfg5Y_d59Ks9xY"
-    var GIqueryURL = "https://www.googleapis.com/customsearch/v1?key=" + GIapi + "&cx=003281430856125788303:ccpcjlqp7kq&q=" + searchValue;
+    var GIqueryURL = "https://www.googleapis.com/customsearch/v1?searchType=image&key=" + GIapi + "&cx=003281430856125788303:ccpcjlqp7kq&q=" + searchValue;
     $.ajax({
         url: GIqueryURL,
         method: "GET",
     }).then(function(response) {
         console.log(response);
-    });
+    
+    $("#googleImages").empty()
+    for (var i = 0; i<response.items.length; i++) {
+        var imgdiv = $("<div class='imgdiv'>")
+        var imglink = $("<a>")
+        imgdiv.append(imglink)
+        imglink.attr("href", response.items[i].link)
+        imglink.attr("target", "_blank")
+        $("#googleImages").append(imgdiv)
 
+        var img = $("<img>")
+        img.attr("src", response.items[i].image.thumbnailLink)
+        img.attr("alt", response.items[i].title)
+        imglink.append(img)
+    }
+    });
 
     var NYTqueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     NYTqueryURL += '?' + $.param({
