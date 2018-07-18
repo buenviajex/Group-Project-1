a6bccd5721caf33dc1bd9f965acfc902d9a3e931
matteichler <matteichler7@gmail.com>
2018-07-17 19:41:59 -0700

    couple ajax calls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,32 @@
 $(".searchButton").on("click", function(event) {
     event.preventDefault();
-    var searchValue= $("#searchInput").val().trim();
-    var queryURL = https://cors-anywhere.herokuapp.com/https://www.google.com/search?tbm=isch&q=" + searchInput + "";
+    var searchValue= $(".searchInput").val().trim();
 
+    var GIapi = "AIzaSyBFF-Z0wLlFirA44q-R_Yfg5Y_d59Ks9xY"
+    var GIqueryURL = "https://www.googleapis.com/customsearch/v1?key=" + GIapi + "&cx=003281430856125788303:ccpcjlqp7kq&q=" + searchValue;
     $.ajax({
-        url: queryURL,
+        url: GIqueryURL,
         method: "GET",
     }).then(function(response) {
         console.log(response);
     });
+
+
+    var NYTqueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    NYTqueryURL += '?' + $.param({
+        'api-key': "ebfd3fb4511c45b8a0f31b81f539debc",
+        'q': searchValue
+    });
+    $.ajax({
+        url: NYTqueryURL,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+    }).fail(function(err) {
+        throw err;
+    });
+
+
 });
  
 // function(response) {
