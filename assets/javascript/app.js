054435b0f9d5e3868ef14d07c07540bb07c82a25
uglymushroom <lifanjin@ucla.edu>
2018-07-17 20:41:43 -0700

    added jquery to move logo and search bar upon pressing submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,33 @@
-$(".searchButton").on("click", function(event) {
-    event.preventDefault();
-    var searchValue= $("#searchInput").val().trim();
-    var queryURL = https://cors-anywhere.herokuapp.com/https://www.google.com/search?tbm=isch&q=" + searchInput + "";
+// $(".searchButton").on("click", function(event) {
+//     event.preventDefault();
+//     var searchValue= $("#searchInput").val().trim();
+//     var queryURL = "https://cors-anywhere.herokuapp.com/https://www.google.com/search?tbm=isch&q=" + searchInput + "";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function(response) {
-        console.log(response);
-    });
-});
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//     }).then(function(response) {
+//         console.log(response);
+//     });
+// });
  
+$(".searchButton").click(function(){
+    $(".mainlogo").removeClass("center-align");
+    $(".mainlogo").addClass("left s6");
+    $(".logoimage").animate({
+        height: '100px',
+        width: '400px'
+    });
+    $("#searchBar").removeClass("s12");
+    $("#searchBar").addClass("searchBar s6");
+    $("#searchBar").animate({
+        size: '50%',
+    });
+
+    // jquery helper to run event.preventDefault() for you;
+    return false;
+});
+
 // function(response) {
 //       addNewRow(response.data);
 //   });
